f34287a1f57cee362a9070b76402f53435976528
Jose Luis Cristobal <jose.cristobal@techie.pe>
2019-11-04 23:03:59 -0500

    Agregar archivos de proyecto.

---
+++ DataAccessLayer/DAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class DAL
+    {
+        static ApiDBEntities DbContext;
+
+        static DAL()
+        {
+            DbContext = new ApiDBEntities();
+        }
+        public static List<Product> GetAllProducts()
+        {
+            return DbContext.Products.ToList();
+        }
+        public static Product GetProduct(int productId) {
+            return DbContext.Products.Where(p => p.ProductId == productId).FirstOrDefault();
+        }
+        public static bool InsertProduct(Product product)
+        {
+            bool status;
+
+            try
+            {
+                DbContext.Products.Add(product);
+                DbContext.SaveChanges();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+        public static bool UpdateProduct(Product product)
+        {
+            bool status;
+
+            try
+            {
+                Product prodItem = DbContext.Products.Where(p => p.ProductId == product.ProductId).FirstOrDefault();
+                if (prodItem != null)
+                {
+                    prodItem.ProductName = product.ProductName;
+                    prodItem.Quantity = product.Quantity;
+                    prodItem.Price = product.Price;
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        public static bool DeleteProduct(int id)
+        {
+            bool status;
+            try
+            {
+                Product product = DbContext.Products.Where(p => p.ProductId == id).FirstOrDefault();
+
+                if (product != null)
+                {
+                    DbContext.Products.Remove(product);
+                    DbContext.SaveChanges();
+                }
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+            }
+            return status;
+        }
+    }
+}
---
+++ WebAPIService/Repository/EntityMapper.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer;
+using AutoMapper;
+
+namespace WebAPIService.Repository
+{
+    public class EntityMapper<TSource, TDestination> where TSource : class where TDestination : class
+    {
+        public EntityMapper()
+        {
+            Mapper.CreateMap<Models.Product, Product>();
+            Mapper.CreateMap<Product, Models.Product>();
+        }
+        public TDestination Translate(TSource obj)
+        {
+            return Mapper.Map<TDestination>(obj);
+        }
+    }
+}
